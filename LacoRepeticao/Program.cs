4d848ce2377f6c2a6cd31305906b85f54d8bf8ee
Renatatr <renatatrintin@gmail.com>
2023-08-16 16:43:21 -0300

    Diferença entre for e foreach

---
+++ LacoRepeticao/Program.cs
@@ -0,0 +1,20 @@
+﻿// Faça como eu fiz: for e foreach
+
+List<int> listaInteiros = new List<int>{2,3,5,56,23,123};
+
+Console.WriteLine("FOR:");
+for (int i = 0; i < listaInteiros.Count; i++)
+{
+    if (listaInteiros[i]%2 == 0)
+    {
+        Console.WriteLine(listaInteiros[i]);
+    }
+}
+Console.WriteLine("FOREACH:");
+foreach (var numero in listaInteiros)
+{
+    if (numero%2 == 0)
+    {
+        Console.WriteLine(numero);
+    }
+}
